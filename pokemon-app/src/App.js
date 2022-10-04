8db539379032d3b7d2b19efc4e1d1254d5ac0123
MizukaNagura <buriki1214@gmail.com>
2022-10-04 22:19:02 +0900

    ポケモン画像がブラウザに出るようになった

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import {getAllPokemon} from "./utils/pokemon.js";
+import Card from './components/Card';
+import {getAllPokemon, getPokemon} from "./utils/pokemon.js";
 
 function App() {
   //エンドポイント　これでポケモンの情報を持ってくる
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true)
+  //_pokemonDataの状態変数を格納
+  const [pokemonData, setPokemonData] = useState([]);
+   
 
 //ブラウザをリロードした時に全部のポケモン情報を取ってくる
 //1回だけ呼び出したいので第2引数は空にする
@@ -15,24 +19,46 @@ useEffect(() => {
   const fetchPokemonData = async () => {
     //全てのポケモンデータを取得
     let res = await getAllPokemon(initialURL);
-    console.log(res);
+     //各ポケモンの詳細なデータを取
+     loadPokemon(res.results);
+     //console.log(res.results);
+    //console.log(res);
     setLoading(false);
   };
   fetchPokemonData();
 }, []);
 
+//res.resultsをdataとして受け取る
+//20種類のポケモンを受け取り終わるまで(fetch)
+//最終的に取得してきたものを_pokemonDataに格納　
+const loadPokemon = async (data) => {
+  let _pokemonData = await Promise.all(
+    data.map((pokemon) => {
+      //console.log(pokemon);
+      //pokemon.urlにはそのポケモンの詳細が入ってる（エンドポイント）
+      let pokemonRecord = getPokemon(pokemon.url);
+      return pokemonRecord;
+    })
+  );
+  setPokemonData(_pokemonData);
+};
 
-
+  console.log(pokemonData);
 
   return (
     <div className="App">
     {loading ? (
       <h1>ロード中</h1>
-    ) : 
-      <h1>ポケモンデータを取得しました！</h1>
-  }
+    ) : (
+        <div className="pokemonCardContainer">
+          {pokemonData.map((pokemon, i) => {
+            //プロップス
+            return <Card key={i} pokemon={pokemon} />;
+          })}
+        </div>
+  )}
   </div>
   );
-}
+};
 
 export default App;
--- pokemon-app/src/utils/pokemon.js
+++ pokemon-app/src/utils/pokemon.js
@@ -3,7 +3,20 @@ export const getAllPokemon = (url) => {
     return new Promise((resolve, reject) => {
         //Promiseオブジェクトにより、下記かコードが成功するまで待たなきゃいけない
         //データがあったら→データをresとして受け取る（JSON形式に変換して）
-        fetch(url).then((res) => res.json())
+        fetch(url)
+        .then((res) => res.json())
         .then((data) => resolve(data));
     });
 };
+//urlという名前でpokemon.urlを受け取ってfetchまで行く
+export const getPokemon = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                resolve(data)
+            });
+    }); 
+};
+
